7c52aaa9c67de44e057afa19c65b907794c99f80
karimliaghabala <agabala.93@gmail.com>
2023-01-26 00:52:03 +0400

    design responsiv

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import './header.css'
-import Logo from '../../assets/logo.png'
+import React,{useState} from 'react';
+import './header.css';
+import Logo from '../../assets/logo.png';
+import Bars from '../../assets/bars.png'; 
 
-const header = () => {
+const Header = () => {
+  const mobile = window.innerWidth<=768 ? true: false;
+  const [menuOpened, setMenuOpened] = useState(false)
   return (
-    <div>
+    <>
       <div className='header'>
+
         <img src={Logo} alt="logo" className='logo' />
+        {(menuOpened === false && mobile ===true )?(
+        <div style={{backgroundColor:"var(--appColor",padding:"0.5rem",borderRadius:"5px"}} onClick={()=>setMenuOpened(true)}>
+          <img src={Bars} alt="" style={{width:"1.5rem", height:"1.5rem"}}/>
+        </div>
+        ): (
         <ul className='header-menu'>
-          <li>Home</li>
-          <li>Programs</li>
-          <li>Why us</li>
-          <li>Plans</li>
-          <li>Testimonials</li>
-        </ul>
+          <li onClickk={()=> setMenuOpened(false)}>Home</li>
+          <li onClickk={()=> setMenuOpened(false)}>Programs</li>
+          <li onClickk={()=> setMenuOpened(false)}>Why us</li>
+          <li onClickk={()=> setMenuOpened(false)}>Plans</li>
+          <li onClickk={()=> setMenuOpened(false)}>Testimonials</li>
+        </ul>)}
       </div>
-    </div>
+    </>
   )
 }
 
-export default header
+export default Header
